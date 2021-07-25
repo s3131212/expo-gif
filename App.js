ca7c9f79bc229ba170ab27f7dbfed9d39599d925
Allen Chou <s3131212@gmail.com>
2021-07-25 22:17:19 +0800

    init

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Test GIF</Text>
+      <Image source={require("./assets/test.gif")} style={{ width: 200, height: 200 }} resizeMode={"contain"} />
     </View>
   );
 }
